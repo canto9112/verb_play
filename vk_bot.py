5dfd18d61bddcef0dce320a34695df99afdca361
canto9112 <cantoweb112@gmail.com>
2021-03-20 10:15:27 +0300

    Высушил код

--- vk_bot.py
+++ vk_bot.py
@@ -5,23 +5,8 @@ import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 import detect_intent_google
 
-def vk_bot(token):
-    vk_session = vk_api.VkApi(token=token)
 
-    longpoll = VkLongPoll(vk_session)
-
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение от:', event.user_id)
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-                print('Текст:', event.text)
-            else:
-                print('От меня для: ', event.user_id)
-                print('Текст:', event.text)
-
-
-def echo(event, vk_api, user_id):
+def send_message(event, vk_api, user_id):
     vk_api.messages.send(
         user_id=user_id,
         message=event,
@@ -49,5 +34,5 @@ if __name__ == "__main__":
 
             fulfillment_text, fallback_intent = detect_intent_google.detect_intent_texts(project_id, user_id, user_message, language)
             if not fallback_intent:
-                echo(fulfillment_text, vk_api, user_id)
+                send_message(fulfillment_text, vk_api, user_id)
 
