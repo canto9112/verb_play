9238e0bbf6c4952dc37abda61d23e9c087475834
canto9112 <cantoweb112@gmail.com>
2021-03-20 11:34:03 +0300

    Оптимизировал импорты

--- vk_bot.py
+++ vk_bot.py
@@ -1,9 +1,9 @@
-from environs import Env
 import random
 
 import vk_api as vk
-from vk_api.longpoll import VkLongPoll, VkEventType
-import config
+from vk_api.longpoll import VkEventType, VkLongPoll
+
+import create_intents
 
 
 def send_message(event, vk_api, user_id):
@@ -14,17 +14,10 @@ def send_message(event, vk_api, user_id):
     )
 
 
-def starting_vk_bot():
-    env = Env()
-    env.read_env()
-    token = env('VK_TOKEN')
-
+def starting_vk_bot(token, project_id, language):
     vk_session = vk.VkApi(token=token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
-
-    language = "ru-RU"
-    project_id = env('DIALOG_FLOW_ID_PROJECT')
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
@@ -32,6 +25,6 @@ def starting_vk_bot():
             user_id = event.user_id
             user_message = event.text
 
-            fulfillment_text, fallback_intent = config.detect_intent_texts(project_id, user_id, user_message, language)
+            fulfillment_text, fallback_intent = create_intents.detect_intent_texts(project_id, user_id, user_message, language)
             if not fallback_intent:
                 send_message(fulfillment_text, vk_api, user_id)
