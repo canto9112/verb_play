a23742af9640c1e8f5462d8ba14d3a8ef0525ac7
canto9112 <cantoweb112@gmail.com>
2021-03-20 10:29:54 +0300

    Удалил функцию логгера

--- telegram_bot.py
+++ telegram_bot.py
@@ -28,17 +28,10 @@ def start_polling(token):
     updater.start_polling()
 
 
-def get_logger():
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
-    logger = logging.getLogger(__name__)
-    return logger
-
-
 if __name__ == '__main__':
     env = Env()
     env.read_env()
-    logger = get_logger()
+    logger = config.get_logger()
 
     token = env('TELEGRAM_BOT_TOKEN')
 
