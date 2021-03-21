d50e94e3b4c8b5baa3c223efc0635fd40c5a7070
canto9112 <cantoweb112@gmail.com>
2021-03-21 09:53:41 +0300

    Заменил Env на  gotenv

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from environs import Env
-
+from dotenv import load_dotenv
 import telegram_bot
 import vk_bot
+import os
 
 
 def main():
-    env = Env()
-    env.read_env()
+    load_dotenv()
 
-    vk_token = env('VK_TOKEN')
-    telegram_token = env('TELEGRAM_BOT_TOKEN')
+    vk_token = os.getenv('VK_TOKEN')
+    telegram_token = os.getenv('TELEGRAM_BOT_TOKEN')
 
     language = "ru-RU"
-    dialog_flow_project_id = env('DIALOG_FLOW_ID_PROJECT')
+    dialog_flow_project_id = os.getenv('DIALOG_FLOW_ID_PROJECT')
 
     telegram_bot.start_telegram_bot(telegram_token, dialog_flow_project_id, language)
     vk_bot.start_vk_bot(vk_token, dialog_flow_project_id, language)
