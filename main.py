6c2b75bbc6388c659e5cba5af7938709d9cff6b7
canto9112 <cantoweb112@gmail.com>
2021-03-21 18:41:40 +0300

    Обновил requirements.txt

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from dotenv import load_dotenv
+from environs import Env
 import telegram_bot
 import vk_bot
-import os
 
 
 def main():
-    load_dotenv()
+    env = Env()
+    env.read_env()
 
-    vk_token = os.getenv('VK_TOKEN')
-    telegram_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    vk_token = env('VK_TOKEN')
+    telegram_token = env('TELEGRAM_BOT_TOKEN')
 
     language = "ru-RU"
-    dialog_flow_project_id = os.getenv('DIALOG_FLOW_ID_PROJECT')
+    dialog_flow_project_id = env('DIALOG_FLOW_ID_PROJECT')
 
     telegram_bot.start_telegram_bot(telegram_token, dialog_flow_project_id, language)
     vk_bot.start_vk_bot(vk_token, dialog_flow_project_id, language)
